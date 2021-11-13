01e9e1859dbf43a4e100aec46c1f3f92b56450b2
Luke Michalakis <michalakisluke@gmail.com>
2021-11-13 14:40:32 -0500

    <Fix user model>

--- models/User.js
+++ models/User.js
@@ -39,17 +39,6 @@ User.init(
         }
     },
     {
-        hooks: {
-        // set up beforeCreate lifecycle "hook" functionality
-            async beforeCreate(newUserData) {
-                newUserData.password = await bcrypt.hash(newUserData.password, 10);
-                return newUserData;
-            },   
-            async beforeUpdate(updatedUserData) {
-                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-                return updatedUserData;
-            }
-        },
         sequelize,
         timestamps: false,
         freezeTableName: true,
