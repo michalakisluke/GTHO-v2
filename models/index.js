4d649148507b547d6b77395ac93d33680647afc2
Luke Michalakis <michalakisluke@gmail.com>
2021-11-16 18:59:08 -0500

    <Start building associations>

---
+++ models/index.js
@@ -0,0 +1,5 @@
+const User = require('./User');
+const Destination = require('./Destination');
+
+//Associations
+//Need to figure this out, Destination needs to reference User somehow I think. Talk to jonathan.
