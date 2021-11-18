90c5ae3079d7d9e28563d439af1425675536c95d
Flumanuck <55293870+Flumanuck@users.noreply.github.com>
2021-11-18 15:08:46 -0500

    added route framework

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const db = require("../models");
+
+//GET routes
+
+router.get("/api/dash", (req, res) => {});
+
+router.get("/api/dash/:id", (req, res) => {});
+
+//POST route
+
+router.post("/api/dash", (req, res) => {});
+
+//DELETE route
+
+router.delete("/api/dash/:id", (req, res) => {});
+
+module.exports = router;
---
+++ controllers/home-routes.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const db = require("../models");
+
+//GET routes
+
+router.get("/api/home", (req, res) => {});
+
+router.get("/api/home/:id", (req, res) => {});
+
+//POST route
+
+router.post("/api/home/", (req, res) => {});
+
+//DELETE route
+
+router.delete("/api/home/:id", (req, res) => {});
+
+module.exports = router;
