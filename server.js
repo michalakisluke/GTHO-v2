1254c8986043872a36875809bb172f560fa862b4
Luke Michalakis <michalakisluke@gmail.com>
2021-11-18 18:33:44 -0500

    <Test file move issues>

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const session = require('express-session');
-//const helpers = require('./utils/helpers');
+const passport = require('passport-local');
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store)
 
