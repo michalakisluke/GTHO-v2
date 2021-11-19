0425d4d4faa7d40185fc8b25339a825c6a4f69ec
Luke Michalakis <michalakisluke@gmail.com>
2021-11-19 15:36:59 -0500

    <Rebuild and restructure controllers file>

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const passport = require('./passport/passport');
-// require('./passport/passport');
+//const passport = require('./passport/passport');
+require('./passport/passport');
 require("dotenv").config();
 const SequelizeStore = require('connect-session-sequelize')(session.Store)
 const app = express();
@@ -24,8 +24,8 @@ const sess = {
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.set("views", "./views");
-// app.use(passport.initialize());
-// app.use(passport.session());
+//app.use(passport.initialize());
+//app.use(passport.session());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -34,7 +34,8 @@ app.use(session(sess));
 //turn on routes
 app.get('/', (req, res) => {
     res.render('index', {});
-})
+});
+
 //turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'))
