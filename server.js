db6e8b0ac3cb05d1f1be5a4d7649732eb8d0d5ac
Luke Michalakis <michalakisluke@gmail.com>
2021-11-19 12:23:36 -0500

    <Fix handlebar load issues>

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const path = require('path');
 const express = require('express');
 const routes = require('./controllers/');
-console.log("here")
 const sequelize = require('./config/connection');
-console.log("here2")
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const passport = require('./passport/passport');
@@ -12,9 +10,7 @@ require("dotenv").config();
 const SequelizeStore = require('connect-session-sequelize')(session.Store)
 const app = express();
 const PORT = process.env.PORT || 9001;
-// sequelize.sync().then(() => {
-//     console.log("HERE")
-// })
+
 const hbs = exphbs.create()
 const sess = {
     secret: 'secret',
@@ -25,23 +21,21 @@ const sess = {
         db: sequelize
     })
 };
-console.log("here3")
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-app.set("views", "./views")
+app.set("views", "./views");
 // app.use(passport.initialize());
 // app.use(passport.session());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session(sess));
-console.log("here4")
+
 //turn on routes
 app.get('/', (req, res) => {
     res.render('index', {});
 })
 //turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-    console.log("here5")
     app.listen(PORT, () => console.log('Now listening'))
 });
