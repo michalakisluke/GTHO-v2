ed505d94f7df214f4f40ef24ed52278ce1d212d0
Sarah Dickinson <sarahadickinson@gmail.com>
2021-11-19 05:53:30 -0500

    minor route changes

--- models/Review.js
+++ models/Review.js
@@ -4,26 +4,22 @@ const sequelize = require('../config/connection');
 // create our User Model
 class Review extends Model {}
 
-Review.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        rating: {
-            type: DataTypes.INTEGER,
-            allowNull: true
-        }  
+Review.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
     },
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'review'
+    rating: {
+        type: DataTypes.INTEGER,
+        allowNull: true
     }
-);
+}, {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'review'
+});
 
 module.exports = Review;
--- server.js
+++ server.js
@@ -3,18 +3,18 @@ const express = require('express');
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const session = require('express-session');
-const exphbs = require('express-handlebars');
+
 const passport = require('passport');
 require('./passport/passport');
 require("dotenv").config();
 
-const SequelizeStore = require('connect-session-sequelize')(session.Store)
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
+const exphbs = require('express-handlebars');
 const hbs = exphbs.create()
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
 
 const sess = {
     secret: process.env.SESSIONS_PW,
@@ -39,7 +39,8 @@ app.set('view engine', 'handlebars');
 //turn on routes
 app.get('/', (req, res) => {
     res.send('Eureka!');
-})
+});
+// app.use(routes);
 
 //turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
