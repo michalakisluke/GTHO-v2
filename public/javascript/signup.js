c47423c657e5071587c7f84ef5acfc6bb6123919
Sarah Dickinson <sarahadickinson@gmail.com>
2021-11-20 10:06:28 -0500

    SOS

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,9 +1,9 @@
 async function signupFormHandler(event) {
     event.preventDefault();
 
-    const username = document.querySelector('#username').value.trim();
-    const email = document.querySelector('#email').value.trim();
-    const password = document.querySelector('#password').value.trim();
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
 
     var userStatus = null;
 
@@ -22,7 +22,7 @@ async function signupFormHandler(event) {
         if (response.ok) {
             userStatus = loggedin;
         } else {
-            $("#password").effect("shake");
+            $("#password-signup").effect("shake");
         }
     }
 };
